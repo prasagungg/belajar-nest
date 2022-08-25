38482563820dc29c242f248d5bae4cd0d4127d81
Agung <agungprasetio@xcidic.com>
2022-08-25 15:16:43 +0700

    sign in

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
-
+import { Prisma } from '@prisma/client';
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
@@ -11,19 +11,46 @@ export class AuthService {
     //generate the password hash
     const hash = await argon.hash(dto.password);
 
-    //save the new user in the db
-    const user = await this.prisma.user.create({
-      data: {
+    try {
+      //save the new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+
+      return user;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+    }
+  }
+  async signin(dto: AuthDto) {
+    // find the User By Email
+    const user = await this.prisma.user.findUnique({
+      where: {
         email: dto.email,
-        hash,
       },
     });
+
+    if (!user) {
+      throw new ForbiddenException('credential incconrect');
+    }
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    if (!pwMatches) {
+      throw new ForbiddenException('credential incconrect');
+    }
 
     delete user.hash;
 
     return user;
-  }
-  signin() {
-    return { msg: 'I have signed in' };
   }
 }
